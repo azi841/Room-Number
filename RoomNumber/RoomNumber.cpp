9b81e23f088eae51d5f9d269c50361961d3e88fd
azi841 <aazzhhaarroo@gmail.com>
2022-12-22 23:52:58 +0100

    Updated version

--- RoomNumber/RoomNumber.cpp
+++ RoomNumber/RoomNumber.cpp
@@ -9,17 +9,13 @@ int RoomNumber::numberOfSets(int roomNumber) {
         return 0;
     }
 
-    int digits = 0;
     int saver = roomNumber;
 
-    //Dividing integer into digits
-    do { roomNumber /= 10; digits++; } while (roomNumber != 0);
-    //Creating array sized of number of digits
-    vector<int> p(digits);
-    //Saving values of digits into array
-    for (int i = digits - 1; i >= 0; i--) {
-        p[i] = saver % 10;
-        saver = saver / 10;
+    //Dividing digits into vector
+    vector<int> digits;
+    while (roomNumber) {
+        digits.push_back(roomNumber % 10);
+        roomNumber /= 10;
     }
 
     int setCounter = 1; //Final counter of sets needed
@@ -27,20 +23,20 @@ int RoomNumber::numberOfSets(int roomNumber) {
     int sixCounter; // Counter for numbers 6 and 9
 
     //Nested loop for checking how many sets are needed for entered integer
-    for (int i = 0; i < digits; i++) {
-        if (p[i] < 0) continue; //Checking if the number is already checked (line 39 and 43)
+    for (int i = 0; i < digits.size(); i++) {
+        if (digits[i] < 0) continue; //Checking if the number is already checked (line 39 and 43)
         loopCounter = 1;
         sixCounter = 2;
 
-        for (int j = i + 1; j < digits; j++) {
-            if (p[j] < 0) continue;
-            if ((p[i] == 6 || p[i] == 9) && (p[j] == 6 || p[j] == 9)) { //Comparison of numbers i and j if i/j is equal to 6 or 9
+        for (int j = i + 1; j < digits.size(); j++) {
+            if (digits[j] < 0) continue;
+            if ((digits[i] == 6 || digits[i] == 9) && (digits[j] == 6 || digits[j] == 9)) { //Comparison of numbers i and j if i/j is equal to 6 or 9
                 sixCounter++;
-                p[j] = -1;
+                digits[j] = -1;
             }
-            else if (p[i] == p[j]) { //Comparison of numbers other than 6 and 9
+            else if (digits[i] == digits[j]) { //Comparison of numbers other than 6 and 9
                 loopCounter++;
-                p[j] = -1;
+                digits[j] = -1;
             }
         }
 
